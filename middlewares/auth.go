36e60d78a43335400a17f3ed2a68ed5f06b5efe9
mkawsar <kawsar.diu888@gmail.com>
2025-02-06 13:22:20 +0600

    auth middleware added for jwt token verify

---
+++ middlewares/auth.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"health/models"
+	db "health/models/db"
+	"health/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// JwtMiddleware is a middleware function for Gin that checks for a Bearer token
+// in the request header. It verifies the token using the VerifyToken service
+// and, if valid, sets the user ID in the context. If the token is invalid, it
+// sends an unauthorized error response and aborts the request.
+func JwtMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("Bearer-Token")
+		tokenModel, err := services.VerifyToken(token, db.TokenTypeAccess)
+		if err != nil {
+			models.SendErrorResponse(ctx, http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		ctx.Set("userIdHex",tokenModel.User.Hex())
+		ctx.Set("userId", tokenModel.User)
+		ctx.Next()
+	}
+}
