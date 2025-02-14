14350edbeb4637943d9613e30f6e997b1d72f2c3
mkawsar <kawsar.diu888@gmail.com>
2025-02-14 20:29:17 +0600

    created websocket hub

--- models/db/hub.go
+++ models/db/hub.go
@@ -1,8 +1,47 @@
 package models
 
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
 type Hub struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
 	register   chan *Client
 	unregister chan *Client
+}
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	hub = newHub()
+)
+
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		}
+	}
 }
