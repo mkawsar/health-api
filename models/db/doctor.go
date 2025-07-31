6c95cd952deb0f6fa83989f90a778e0e62bc9592
mkawsar <kawsar.diu888@gmail.com>
2025-07-31 12:05:15 +0600

    resolve doctor model structure

--- models/db/doctor.go
+++ models/db/doctor.go
@@ -5,8 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Doctor struct {
 	ID primitive.Binary `bson:"_id"`
 	Name string `bson:"name"`
-	Email string `bson:"email"`
-	Password string `bson:"password"`
 	Specialization string `bson:"specialization"`
 	Phone string `bson:"phone"`
 	Experience string `bson:"experience"`
@@ -19,11 +17,9 @@ type Doctor struct {
 	WorkTimeEnd []string `bson:"work_time_end"`
 }
 
-func NewDoctor(name string, email string, password string, specialization string, phone string, experience string, location string, license string, workHours string, availability bool, workDays []string, workTime []string, workTimeEnd []string) *Doctor {
+func NewDoctor(name string, specialization string, phone string, experience string, location string, license string, workHours string, availability bool, workDays []string, workTime []string, workTimeEnd []string) *Doctor {
 	return &Doctor{
 		Name: name,
-		Email: email,
-		Password: password,
 		Specialization: specialization,
 		Phone: phone,
 		Experience: experience,
